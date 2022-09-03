316a1ec444cc2c0ed97aed2817a55cdef19c2ccb
Petzi <daboarderpjb@gmail.com>
2022-09-03 14:44:46 +0200

    auto id

--- src/main/java/com/example/sqlsandbox/product/ProductItem.java
+++ src/main/java/com/example/sqlsandbox/product/ProductItem.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -22,6 +25,8 @@ public class ProductItem {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     @Column(name = "id", nullable = false)
     private String id;
     private String name;
